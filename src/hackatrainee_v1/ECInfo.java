d35f47532cfa9f258bab0e155e2b3ae57d5da943
Jelle Nauta <jelle.nauta@tno.nl>
2021-01-18 22:08:04 +0100

    Report conviction of ECs

--- src/hackatrainee_v1/ECInfo.java
+++ src/hackatrainee_v1/ECInfo.java
@@ -4,10 +4,12 @@ import battlecode.common.MapLocation;
 import battlecode.common.Team;
 
 public class ECInfo {
-	public ECInfo(MapLocation location, Team team) {
+	public ECInfo(MapLocation location, Team team, int conviction) {
 		this.location = location;
 		this.team = team;
+		this.conviction = conviction;
 	}
 	MapLocation location;
 	Team team;
+	int conviction;
 }
